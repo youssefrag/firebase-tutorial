b035eea57aaab32f6c917b00ef89e566f893c93f
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-21 18:50:26 -0400

    initialize firebase app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello from index.js");
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNCnyNrqAabPOJKs5X3lvMvUn-aXj1R5c",
+  authDomain: "fir-9-dojo-f67aa.firebaseapp.com",
+  projectId: "fir-9-dojo-f67aa",
+  storageBucket: "fir-9-dojo-f67aa.appspot.com",
+  messagingSenderId: "42178163007",
+  appId: "1:42178163007:web:f93cd00de7d8d99f4b238a",
+};
+
+initializeApp(firebaseConfig);
